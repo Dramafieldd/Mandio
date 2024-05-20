ddb0270cb71f3a1450bd2125a4551e4bf9d910a1
Dramafieldd <alicia.chen230@gmail.com>
2024-05-20 16:33:51 +0700

    Initial Commit

---
+++ Mandio/MandioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandioApp.swift
+//  Mandio
+//
+//  Created by Alicia Laurence on 20/05/24.
+//
+
+import SwiftUI
+
+@main
+struct MandioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
